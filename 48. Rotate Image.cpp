100aa29254dc8e8a6c159791a169b9bdbcbfe144
Yuuta Huang <91250050+yuutahuang@users.noreply.github.com>
2023-05-02 11:36:56 +0800

    Update 48. Rotate Image.cpp

--- 48. Rotate Image.cpp
+++ 48. Rotate Image.cpp
@@ -12,13 +12,13 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        // ¹ï¨¤½u¥æ´«
+        //å°è§’ç·šäº¤æ›
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        // ¨C¤@¦æÂ½Âà
+        //æ¯ä¸€è¡Œç¿»è½‰
         for (int i = 0; i < n; i++) {
             reverse(matrix[i].begin(), matrix[i].end());
         }
